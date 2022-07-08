ac3a7358154114a0d7a9fcd5e791b698ebfe3144
Сергей <99472918+Stalexus@users.noreply.github.com>
2022-07-08 15:51:51 +0300

    feat(react-pover): add navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,14 @@
 import './styles.scss';
+import logo from "../assets/applelogo.png";
 
 export const Navbar = () => {
   return (
-    <nav></nav>
+    <nav className="navbar">
+      <img className="navbar__logo" src={logo} alt="applelogo"/>
+      <a className="navbar__link_features" href="#">Features</a>
+      <a className="navbar__link_partners" href="#">Partners</a>
+      <a className="navbar__link_stories" href="#">Stories</a>
+      <button className="navbar__btn">Download for free</button>
+    </nav>
   )
 }
