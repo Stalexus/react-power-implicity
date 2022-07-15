425d6c59ae04db5d4f667aefe1770fa9cc760a43
Сергей <99472918+Stalexus@users.noreply.github.com>
2022-07-15 12:27:49 +0300

    refactor(react-pover): aligned code

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { Slider } from './components/Slider/Slider';
 export const App = () => {
   return (
     <div className="container">
-    <Navbar />
-    <Slider />
+      <Navbar />
+      <Slider />
     </div>
   );
 }
